3173dbb5175eab34d2e60a619bee92d6715b357a
Luis David Vitaliti <luisda8193@hotmail.com>
2024-03-15 13:00:52 -0300

    Modifique script

---
+++ Assets/Scripts/MiPrimerScript.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections; //Namespace,
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiPrimerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Estoy iniciando...");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
