97b44c8bdc21a94c9b12a74b836417320e38c97e
Nicolás M. Perez <nicomati0514@gmail.com>
2024-03-15 13:06:58 -0300

    Clase 15/3

---
+++ Assets/Scripts/MiPrimerScript.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections; //NameSpace, 
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiPrimerScript : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hola Mundo");
+        Debug.Log("Estoy iniciando");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
