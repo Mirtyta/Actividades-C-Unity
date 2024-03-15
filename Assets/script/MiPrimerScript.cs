466f7a16f6f3f9c4b2108c2039ece3dcee35db30
Abraham <ru.a.i.r.bin@gmail.com>
2024-03-15 18:48:36 -0300

    Funciona la consola
    luego de 5 hs de probar funciona el mensaje de la consola: Debug.Log("");

---
+++ Assets/script/MiPrimerScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiPrimerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Conseole On: Estoy iniciando...");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
