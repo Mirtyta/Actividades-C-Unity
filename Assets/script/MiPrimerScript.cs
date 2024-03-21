fe4577feb6e9634ad7ddc64e226572f949ea08c7
Abraham <ru.a.i.r.bin@gmail.com>
2024-03-21 11:15:57 -0300

    Debug Log
    Se agrego la line del debug log que se dejo como tarea, así como la imagen.

--- Assets/script/MiPrimerScript.cs
+++ Assets/script/MiPrimerScript.cs
@@ -7,7 +7,7 @@ public class MiPrimerScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Conseole On: Estoy iniciando...");
+        Debug.Log("Console On: Estoy iniciando...");
     }
 
     // Update is called once per frame
